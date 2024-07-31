48ea7318be84bfb816394181c7b72becdc92214a
JoseSandovalVilla <jsvilla776@gmail.com>
2024-07-30 21:30:47 -0600

    api nombre

---
+++ src/main/java/com/example/nombreee/NombreeeApplication.java
@@ -0,0 +1,13 @@
+package com.example.nombreee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NombreeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NombreeeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/nombreee/NombreeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nombreee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NombreeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
